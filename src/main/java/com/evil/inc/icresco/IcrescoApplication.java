903809199080b89c3cc2726cedf7d53fde15763f
ipascari <jhoonnyc@gmail.com>
2021-09-12 18:01:02 +0300

    add the project skeleton

---
+++ src/main/java/com/evil/inc/icresco/IcrescoApplication.java
@@ -0,0 +1,13 @@
+package com.evil.inc.icresco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcrescoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcrescoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evil/inc/icresco/IcrescoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evil.inc.icresco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcrescoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
